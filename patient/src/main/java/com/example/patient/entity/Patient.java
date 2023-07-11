bb7c6c2748252368af88ca7abedd7b7e9d013b17
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-07-11 02:13:33 +0200

    add initial patients

---
+++ patient/src/main/java/com/example/patient/entity/Patient.java
@@ -0,0 +1,51 @@
+package com.example.patient.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+@Table(name = "patients")
+@Entity
+public class Patient implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String family;
+
+    @Column(nullable = false)
+    private String given;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dob;
+
+    @Column(nullable = false)
+    private Gender sex;
+
+    private String address;
+
+    private String phone;
+
+    public enum Gender {
+        M,
+        F,
+        O
+    }
+
+
+
+
+}
