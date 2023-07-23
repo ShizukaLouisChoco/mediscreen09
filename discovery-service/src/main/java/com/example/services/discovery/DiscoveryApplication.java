15280aa283f14a56fe2e5c06e9bb7db3c9eacd93
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-07-23 23:51:44 +0200

    Creation of discovery-service module

--- discovery-service/src/main/java/com/example/services/discovery/DiscoveryApplication.java
+++ discovery-service/src/main/java/com/example/services/discovery/DiscoveryApplication.java
@@ -1,15 +1,15 @@
-package com.example.eurekaserver;
+package com.example.services.discovery;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(DiscoveryApplication.class).run(args);
     }
 
 }
