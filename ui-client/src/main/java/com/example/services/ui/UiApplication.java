aec8bea880c0e6d80620ac061e66614def4b22a8
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-07-31 22:27:00 +0200

    activate FeignClients for UiApplication

--- ui-client/src/main/java/com/example/services/ui/UiApplication.java
+++ ui-client/src/main/java/com/example/services/ui/UiApplication.java
@@ -4,6 +4,7 @@ package com.example.services.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.services.ui")
