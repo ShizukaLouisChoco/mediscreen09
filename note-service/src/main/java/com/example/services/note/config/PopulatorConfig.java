c16404a3b34e71aab22d5bc5a94214feb161a070
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-07-31 17:31:05 +0200

    add populator for data insert

---
+++ note-service/src/main/java/com/example/services/note/config/PopulatorConfig.java
@@ -0,0 +1,27 @@
+package com.example.services.note.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+
+@Configuration
+public class PopulatorConfig {
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        factory.setMapper(objectMapper);
+        factory.setResources(new Resource[]{
+                new ClassPathResource("notes.json")
+        });
+        return factory;
+    }
+}
