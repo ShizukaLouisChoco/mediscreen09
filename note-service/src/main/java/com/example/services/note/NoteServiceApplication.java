8a314ca5297db9146d0cacbaa643df95bcd430e2
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-07-31 17:32:24 +0200

    add eurka client, config-service settings

---
+++ note-service/src/main/java/com/example/services/note/NoteServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.services.note;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info =
+@Info(title = "Patient API", version = "1.1", description = "Documentation Patient API v1.1"))
+public class NoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteServiceApplication.class, args);
+	}
+
+}
