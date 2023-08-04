05b6a19112d5d256e9a4608b7c9bd26fb0781a5e
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-08-04 14:19:38 +0200

    creation of report-service module

--- report-service/src/main/java/com/mediscreen/services/report/ReportServiceApplication.java
+++ report-service/src/main/java/com/mediscreen/services/report/ReportServiceApplication.java
@@ -1,9 +1,16 @@
 package com.mediscreen.services.report;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info =
+@Info(title = "Report API", version = "1.1", description = "Documentation Report API v1.1"))
+
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
