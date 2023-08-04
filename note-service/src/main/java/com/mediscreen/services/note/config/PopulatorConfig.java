ad386a9b66ef33054c3b5e415d50cc90111888f1
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-08-04 14:18:27 +0200

    correction PopulatorConfig to reset repository data

--- note-service/src/main/java/com/mediscreen/services/note/config/PopulatorConfig.java
+++ note-service/src/main/java/com/mediscreen/services/note/config/PopulatorConfig.java
@@ -3,6 +3,7 @@ package com.mediscreen.services.note.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mediscreen.services.note.repository.NoteRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,8 +13,15 @@ import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFacto
 @Configuration
 public class PopulatorConfig {
 
+    private final NoteRepository noteRepository;
+
+    public PopulatorConfig(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
     @Bean
     public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+        noteRepository.deleteAll();
         Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
