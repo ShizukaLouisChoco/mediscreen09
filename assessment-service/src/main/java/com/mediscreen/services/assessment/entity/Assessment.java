c4a2c24e9509deed2c5dcc4ce79ea8a8d90e64c6
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-08-16 23:53:09 +0200

    creation of assessment-service module

---
+++ assessment-service/src/main/java/com/mediscreen/services/assessment/entity/Assessment.java
@@ -0,0 +1,15 @@
+package com.mediscreen.services.assessment.entity;
+
+public enum Assessment {
+
+        None,
+        Borderline,
+
+        In_danger,
+
+        Early_onset,
+
+        Error
+    }
+
+
