9f35d8d1cdefa4914395617dc0c18fe8c9f15d44
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-08-17 19:35:26 +0200

    add assessment ui

---
+++ client-ui/src/main/java/com/mediscreen/services/clientui/proxy/AssessmentProxy.java
@@ -0,0 +1,20 @@
+package com.mediscreen.services.clientui.proxy;
+
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "assessment-service", url = "localhost:9200")
+@LoadBalancerClient(name = "assessment-service")
+public interface AssessmentProxy {
+
+
+    @PostMapping("/assess/id")
+    ResponseEntity<String> assessById(@RequestParam("patId") Long patientId);
+
+    @PostMapping("/assess/familyName")
+    ResponseEntity<String> assessByFamilyName(@RequestParam("familyName") String familyName);
+
+    }
--- client-ui/src/main/java/com/mediscreen/services/clientui/proxy/PatientProxy.java
+++ client-ui/src/main/java/com/mediscreen/services/clientui/proxy/PatientProxy.java
@@ -27,6 +27,10 @@ public interface PatientProxy {
     @GetMapping(value ="/patients/{id}")
     PatientBean getPatient(@PathVariable Long id);
 
+    //READ ONE
+    @GetMapping("/patients/family/{family}")
+    PatientBean getPatientByFamily(@PathVariable String family);
+
     //UPDATE
     @PutMapping(value ="/patients/{id}")
     PatientBean updatePatient(@PathVariable Long id, @RequestBody PatientBean patient);
