0479ed6e529d3f28d3a7a9e1f99d81e400314c31
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-09-08 16:49:13 +0200

    update docker-compose.yml

--- assessment-service/src/main/java/com/mediscreen/services/assessment/proxy/PatientProxy.java
+++ assessment-service/src/main/java/com/mediscreen/services/assessment/proxy/PatientProxy.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "patient-service")
 @LoadBalancerClient(name ="patient-service")
-@FeignClient(name = "patient-service", url = "${spring.cloud.openfeign.client.config.patient-service.url}")
 public interface PatientProxy {
 
     @GetMapping("/patients/{id}")
